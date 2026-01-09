deca36467d42729b497a5f2d85f2ebb4e0c912ad
GuilhermeRanquine <guilhermemacedoranquine2003@gmail.com>
2026-01-09 14:44:06 -0300

    feat: implement account creation functionality

---
+++ create_account.py
@@ -0,0 +1,16 @@
+import csv
+
+def create_account():
+    print("--- Create Account ---")
+    name = input("Name: ")
+    surname = input("Surname: ")
+    age = input("Age: ")
+    cpf = input("CPF: ")
+    email = input("Email: ")
+    password = input("Password: ")
+    
+    with open('accounts.csv', "a", newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([name, surname, age, cpf, email, password])
+    print(f"{name}, Account created successfully!")
+    
